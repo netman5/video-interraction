19826c1e261112f9158137331e35e474b30a12e1
Ola Ishola <olaishola@hotmail.co.uk>
2022-09-22 10:35:16 +0100

    Create routes with token for basic auth

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login/Login';
+
+function setToken(userToken) {
+  localStorage.setItem('token', JSON.stringify(userToken));
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const sessionTokenString = sessionStorage.getItem('token');
+  const localStorageTokenString = localStorage.getItem('token');
+  const sessionUserToken = JSON.parse(sessionTokenString);
+  return { session: sessionUserToken?.token, local: localStorageTokenString?.token };
+}
 
 function App() {
+  const token = getToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
+  const logout = () => {
+    setToken('');
+    window.history.pushState({}, '', '/login');
+    window.location.reload();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create a drag and drop video experience</h1>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="login" element={<Login setToken={setToken} />} />
+      </Routes>
+      <button type="button" onClick={logout}>Logout</button>
     </div>
   );
 }
