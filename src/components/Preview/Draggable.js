797d9391c88a536ec650ec8ac36bd6ec35a2d090
Ola Ishola <olaishola@hotmail.co.uk>
2022-09-23 10:52:40 +0100

    Create state for managing newly created elements

--- src/components/Preview/Draggable.js
+++ src/components/Preview/Draggable.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 function Draggable() {
+  const [dragElements, setDragElements] = useState([]);
+
+  // const dragItem = useRef();
+  // const dragOverItem = useRef();
+  const createElemnt = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const element = createElemnt.current.value;
+    setDragElements((current) => [...current, ...[element]]);
+    createElemnt.current.value = '';
+  };
+
+  console.log(dragElements);
+
   return (
     <div className="drag">
       <h2>Create draggable elements</h2>
+      <div className="elements">
+        <input
+          type="text"
+          placeholder="create new element"
+          ref={createElemnt}
+        />
+        <button type="button" onClick={(e) => handleSubmit(e)} className="btn btn-outline-primary">Create Element</button>
+      </div>
     </div>
   );
 }
--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Draggable from './Draggable';
 import VideoPreview from './VideoPreview';
 
 function Preview() {
+  const navigate = useNavigate();
   const location = useLocation();
   const data = location.state;
 
   return (
-    <div className="preview">
-      <Draggable />
-      <VideoPreview data={data} />
+    <div>
+      <button type="button" onClick={() => navigate(-1)} className="btn btn-outline-secondary">Go back</button>
+      <div className="preview">
+        <Draggable />
+        <VideoPreview data={data} />
+      </div>
     </div>
   );
 }
