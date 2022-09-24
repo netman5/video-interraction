5d3cc12198cd6ffecbbff1ca4f25fb6d75e8492c
Ola Ishola <olaishola@hotmail.co.uk>
2022-09-24 19:27:03 +0100

    Creating elements using React.createElement

--- src/components/Preview/Draggable.js
+++ src/components/Preview/Draggable.js
@@ -8,17 +8,34 @@ function Draggable() {
   // const dragItem = useRef();
   // const dragOverItem = useRef();
   const createElemnt = useRef();
+  const elementType = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const element = createElemnt.current.value;
+    const type = elementType.current.value;
     const elementObj = {
       id: uuidv4(),
       title: element,
+      type,
     };
     setDragElements([...dragElements, elementObj]);
     createElemnt.current.value = '';
+    elementType.current.value = '';
   };
+
+  // const createElement = (type, title) => {
+  //   switch (type) {
+  //     case 'text':
+  //       return <p>{title}</p>;
+  //     case 'image':
+  //       return <img src={title} alt="img" />;
+  //     default:
+  //       return <p>{title}</p>;
+  //   } // end switch
+  // };
+
+  const createElement = (type, title) => React.createElement(type, {}, title);
 
   return (
     <div className="drag">
@@ -29,14 +46,25 @@ function Draggable() {
           placeholder="create new element"
           ref={createElemnt}
         />
-        <button type="button" onClick={(e) => handleSubmit(e)} className="btn btn-outline-primary">Create Element</button>
+        <input
+          type="text"
+          placeholder="Element type h1, h2, h3, p, span"
+          ref={elementType}
+        />
+        <button
+          type="button"
+          onClick={(e) => handleSubmit(e)}
+          className="btn btn-outline-primary"
+        >
+          Create Element
+        </button>
       </div>
 
       <div>
         {dragElements && dragElements.map((el) => (
-          <ul key={el.id}>
-            <li>{el.title}</li>
-          </ul>
+          <div key={el.id} draggable>
+            {createElement(el.type, el.title)}
+          </div>
         ))}
       </div>
     </div>
